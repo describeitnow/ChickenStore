9148177bd24e0d4c39945b085b314bcef79a135f
Cat Hanbit <describeitnow@gmail.com>
2023-05-07 17:16:33 +0900

    데이터 분석

---
+++ 서대문구_동별_치킨집_분포.R
@@ -0,0 +1,21 @@
+library("readxl")
+ck <- read_excel("서울서대문구치킨.xlsx")
+head(ck)
+
+addr <- substr(ck$소재지전체주소, 11, 16)
+head(addr)
+
+addr_num <- gsub("[0-9]", "", addr)
+addr_trim <- gsub(" ", "", addr_num)
+head(addr_t)
+
+library(dplyr)
+addr_count <- addr_trim %>% table() %>% data.frame()
+head(addr_count)
+
+install.packages("treemap")
+library(treemap)
+
+treemap(addr_count, index = ".", vSize = "Freq", title = "서대문구 동별 치킨집 분포")
+
+arrange(addr_count, desc(Freq)) %>% head()
